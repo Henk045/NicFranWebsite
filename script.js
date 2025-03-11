d32baff883dd8473a07655fe1b63c7e61d7dbaf7
Henk045 <76645026+Henk045@users.noreply.github.com>
2025-03-11 19:11:45 +0100

    New and tested, might contain some bugs

--- script.js
+++ script.js
@@ -15,48 +15,6 @@ window.addEventListener("scroll", function () {
     }
 });
 
-const toggleFullScreen = (photoFrame) => {
-    const isFullScreen = photoFrame.classList.toggle("full-width");
-    document.body.style.overflow = isFullScreen ? "hidden" : "";
-    document.body.style.position = isFullScreen ? "fixed" : "";
-    document.body.style.width = isFullScreen ? "100%" : "";
-    document.querySelector(".header").style.zIndex = isFullScreen ? "1" : "";
-    document.querySelector(".topnav").style.zIndex = isFullScreen ? "1" : "";
-    window.scrollTo({
-        top: photoFrame.getBoundingClientRect().top + window.pageYOffset - document.querySelector(".header").offsetHeight
-    });
-};
-
-if (window.innerWidth >= 768) {
-    document.querySelectorAll(".photo-frame").forEach(photoFrame => {
-        photoFrame.addEventListener("click", () => toggleFullScreen(photoFrame));
-    });
-}
-
-const toggleFullScreenPhone = (photoFrame) => {
-    const isFullScreen = photoFrame.classList.toggle("full-width");
-    if (isFullScreen) {
-        document.body.style.overflow = "hidden";
-        document.body.style.position = "fixed";
-        document.body.style.width = "100%";
-        document.querySelector(".header").style.zIndex = "1";
-        document.querySelector(".topnav").style.zIndex = "1";
-        photoFrame.querySelector("img").style.transform = "scale(2.0)";
-    } else {
-        document.body.style.overflow = "";
-        document.body.style.position = "";
-        document.body.style.width = "";
-        document.querySelector(".header").style.zIndex = "";
-        document.querySelector(".topnav").style.zIndex = "";
-        photoFrame.querySelector("img").style.transform = "";
-    }
-};
-
-if (window.innerWidth < 768) {
-    document.querySelectorAll(".photo-frame").forEach(photoFrame => {
-        photoFrame.addEventListener("click", () => toggleFullScreenPhone(photoFrame));
-    });
-}
 
 const photos = document.querySelectorAll('.photo');
 const lightbox = document.getElementById('lightbox');
@@ -68,6 +26,7 @@ photos.forEach((photo, index) => {
         lightbox.style.display = 'flex';
         lightboxImg.src = photo.src;
         currentPhotoIndex = index;
+        document.querySelector(".topnav").style.zIndex = "1"; // Prevent topnav from going over the picture
     });
 });
 
@@ -84,5 +43,6 @@ function changePhoto(direction) {
 lightbox.addEventListener('click', (e) => {
     if (e.target !== lightboxImg && !e.target.classList.contains('arrow')) {
         lightbox.style.display = 'none';
+        document.querySelector(".topnav").style.zIndex = ""; // Reset topnav zIndex when lightbox is closed
     }
 });
